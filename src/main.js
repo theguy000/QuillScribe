05dcc43f7460a04bb37ced1984fa96e7851d28a3
theguy000 <istiakm30@gmail.com>
2026-03-26 14:46:17 +0600

    Disable right-click context menu across the application

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+// Disable right-click context menu
+document.addEventListener('contextmenu', (e) => e.preventDefault())
+
 const app = mount(App, {
   target: document.getElementById('app'),
 })
